e9f8535c57a0a402c8191ef9b6b78d234753766e
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-05-11 13:23:28 -0700

    get request when homepage is visited

--- controllers/homeRoutes/homeRoutes.js
+++ controllers/homeRoutes/homeRoutes.js
@@ -1,12 +1,22 @@
 const router = require('express').Router();
-const {  } = require('../../models');
+const { User, Blog } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     try {
-        res.render('homepage', {
-            logged_in: req.session.logged_in
-        })
+        const blogData = await Blog.findAll();
+
+        if (!blogData) {
+            res.status(404).json({ message: 'There are no blogs' });
+        }
+
+        const blogs = Blog.map({ plain: true });
+
+        res.render('homepage', 
+            blogs,
+            {
+                logged_in: req.session.logged_in
+            });
     }catch (err) {
         res.status(500).json(err);
     }
