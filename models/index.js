53ae3d45af4fd3ed7f1313dd30a6aced23a70d6f
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-05-11 12:17:58 -0700

    informed model index.js that Users can have many Blogs and Comments

--- models/index.js
+++ models/index.js
@@ -2,7 +2,25 @@ const User = require('./User');
 const Blog = require('./Blog');
 const Comment = require('./Comment');
 
+User.hasMany(Blog, {
+    foreignKey: 'user_name',
+    onDelete: 'CASCADE'
+});
 
+Blog.belongsTo(User, {
+    foreignKey: 'user_name',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_name',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_name',
+    onDelete: 'CASCADE'
+});
 
 module.exports = {
     User,
