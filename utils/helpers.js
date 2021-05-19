fbdc8b30d742c87a684d6ed1e323853fda9df43f
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-05-19 10:43:09 -0700

    got date to render correctly when blogs are rendered on homepage; working on limiting it to 3

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,7 +5,15 @@ module.exports = {
         new Date(date).getFullYear()
         }`;
     }, 
+    limitBlogs: (arr, max) => {
+        const blog = [];
+
+        for (let i = 0; i < max && i < arr.length; i++) {
+            blog.push(arr[i]);
+        }
+        return blog;
+    },
 };   
 // Using JavaScript Date methods, we get and format the month, date, and year
-    // We need to add one to the month since it is returned as a zero-based value
+    // We need to add one to the month since it is returned as a zero-based value 
   
