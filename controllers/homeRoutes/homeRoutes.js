40efcaf1b440cf9cbd3cba928cbd131be63690f2
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-05-20 11:39:52 -0700

    got rid of withAuth in homepage get request

--- controllers/homeRoutes/homeRoutes.js
+++ controllers/homeRoutes/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Blog, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const userData = await User.findAll({
             include: [{ model: Blog, attributes: ['id', 'title', 'contents', 'date']}]
