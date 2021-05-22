b0b5dfa5a632384c944f1bf17bdb6a631c341286
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-05-21 21:16:58 -0700

    comments on userRoutes.js file

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+//finds a certain user by name and logs them in if they exist, otherwise notifies them that login failed
 router.post('/login', async (req, res) => {
     try {
         const userData = await User.findOne({
@@ -31,6 +32,7 @@ router.post('/login', async (req, res) => {
     }
 });
 
+//creates a new user in database and logs them in
 router.post('/signup', async (req, res) => {
     try {
         const userData = await User.create(req.body);
@@ -44,6 +46,7 @@ router.post('/signup', async (req, res) => {
     }
 });
 
+//logs user out and destroys session
 router.post('/logout', withAuth, (req, res) => {
     if(req.session.logged_in){
         req.session.destroy(() => {
