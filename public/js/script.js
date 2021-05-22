906f3e1cbd934f38ad6906647c59ba8c32d696fb
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-05-21 20:48:11 -0700

    cleaned up script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,4 @@
-// document.querySelector('.oneBlog').style.display = 'none';
-// document.querySelector('.commentBox').style.display = 'flex';
-// document.querySelector('.postedComment').style.display = 'none';
-
 const logoutFn = async () => {
-
-    console.log('See me');
     const response = await fetch('/api/user/logout', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -18,34 +12,16 @@ const logoutFn = async () => {
 
 };
 const singleBlogFn = async (id) => {
-    console.log('id', id);
     const response = await fetch(`/blog/${ id }`, {
         method: 'GET',
-        // headers: { 'Content-Type': 'application/json'}
     });
 
     if (response.ok) {
         document.location.replace(`/blog/${ id }`);
-        // console.log("It's working!");
     } else {
         alert(response.statusText);
     }
 };
-
-// const createComment = async (blog_id, content) => {
-
-//     const response = await fetch('/api/comment', {
-//         method: 'POST',
-//         body: JSON.stringify({ blog_id, content }),
-//         headers: { 'Content-Type': 'application/json'},
-//     });
-
-//     if (response.ok) {
-//         // renderComment(blog_id, content);
-//     } else {
-//         alert(response.statusText);
-//     }
-// };
 
 document.querySelector('#logout').addEventListener('click', logoutFn);
 
@@ -58,15 +34,3 @@ for (let i = 0; i < blogs.length; i++) {
         singleBlogFn(blog_id); 
     });
 };
-
-// document.querySelector('.commentBtn').addEventListener('click', (event) => {
-
-//     const target = event.target;
-//     const blog_id = target.dataset.blog_id;
-//     const content = document.querySelector('.commentText').value.trim();
-//     document.querySelector('.commentBox').style.display = 'none';
-//     document.querySelector('.postedComment').style.display = 'flex';
-//     document.querySelector('.commentContent').innerHTML = content;
-//     createComment(blog_id, content);
-
-// });
