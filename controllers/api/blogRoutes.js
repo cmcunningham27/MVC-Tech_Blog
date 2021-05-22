4ad4fbc90023cc207654ff8ccc8894477999a81e
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-05-21 20:54:56 -0700

    cleaned up blogRoutes.js file

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -3,7 +3,6 @@ const { Blog } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
-    console.log(req.body);
     try {
         const blogData = await Blog.create({
             ...req.body,
@@ -12,7 +11,6 @@ router.post('/', withAuth, async (req, res) => {
 
         res.status(200).json(blogData);
     } catch (err) {
-        console.log(err);
         res.status(400).json(err);
     }
 });
@@ -31,7 +29,6 @@ router.put('/:id', withAuth, async (req, res) => {
 
         res.status(200).json(blogData);
     } catch (err) {
-        console.log(err);
         res.status(500).json(err);
     }
 });
@@ -50,7 +47,6 @@ router.delete('/:id', withAuth, async (req, res) => {
 
         res.status(200).json(blogData);
     } catch (err) {
-        console.log(err);
         res.status(500).json(err);
     }
 })
