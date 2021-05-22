240741b560192f5b5029e429e3b84930eee43fd4
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-05-21 20:43:30 -0700

    removed limitBlogs in the helpers.js as I no longer was using it

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,18 +4,5 @@ module.exports = {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
         new Date(date).getFullYear()
         }`;
-    }, 
-    limitBlogs: (arr, max) => {
-        const blog = [];
-
-        for (let i = 0; i < max && i < arr.length; i++) {
-            blog.push(arr[i]);
-            console.log(i);
-        }
-        console.log(blog);
-        return blog;
-    },
-};   
-// Using JavaScript Date methods, we get and format the month, date, and year
-    // We need to add one to the month since it is returned as a zero-based value 
-  
+    }
+};     
