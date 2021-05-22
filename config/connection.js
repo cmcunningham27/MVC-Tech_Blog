620c51a8da2df47a442be6907cc85bd493dca7ba
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-05-21 21:51:17 -0700

    updated connection.js host for heroku

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: 'localhost',
+        host: process.env.DB_HOST || 'localhost',
         dialect: 'mysql',
         port: 3306,
     }
