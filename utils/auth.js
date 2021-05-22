63807d07f4d93969732ea063da3c6e147bb81c08
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-05-21 20:58:09 -0700

    comment on withAuth.js file

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,4 @@
+// checks to make sure the user is logged-in otherwise redirects them to login page
 const withAuth = (req, res, next) => {
     if (!req.session.logged_in) {
         res.redirect('/login');
